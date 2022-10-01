c46a54ce82fc0d8d51c2b2116f6d8108b5c245d1
Andrey Volynets <voland.artist@gmail.com>
2022-10-01 12:35:14 +0400

    Переписал заполнение массива

--- 29/Program.cs
+++ 29/Program.cs
@@ -1,4 +1,4 @@
-﻿int getNumberFromUser()
+int getNumberFromUser()
 {
     int result = 0;
     while (!int.TryParse(Console.ReadLine(), out result) || (result < 1 ))
@@ -6,6 +6,16 @@
         Console.WriteLine($"Введите натуральное число.");
     }
     return result;
+}
+
+int[] getArray(int arrayLength)
+{
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < arrayLength; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
 }
 
 void printArray(int[] array)
@@ -19,11 +29,13 @@ void printArray(int[] array)
     Console.Write("]");
 }
 
-Console.WriteLine($"Введите длину массива");
+Console.WriteLine("Введите длину массива");
 int arrayLength = getNumberFromUser();
-int[] array = new int[arrayLength];
-for(int i = 0; i < array.Length; i++)
-{
-    array[i] = new Random().Next(1, 100);
-}
-printArray(array);
+//разницу кроме того что выделил в функцию не понял, буквально тот же код -_-
+// int[] newArray = new int[arrayLength];
+// for(int i = 0; i < newArray.Length; i++)
+// {
+//     newArray[i] = new Random().Next(1, 100);
+// }
+int[] newArray = getArray(arrayLength);
+printArray(newArray);
